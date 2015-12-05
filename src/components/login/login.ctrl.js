8df33181216bc3e68385d6b2e40a9dd5a5d33034
Daniel Staudigel <dstaudigel@gmail.com>
2015-12-05 15:29:35 -0800

    Simplistic login controller.

---
+++ src/components/login/login.ctrl.js
@@ -0,0 +1,60 @@
+// LoginCtrl:
+// LoginCtrl is a simple way to manage login forms, because, let's face it,
+// they're all pretty similar.  The idea is to provide login and password
+// reset functionality.
+//
+// Ins:
+//
+// controller.login(hash) and controller.recoverPassword(hash) do what you
+// would think, and return promises that resolve or reject depending on what
+// happens. You can pay close attention tot the resolutions or rejections in
+// your code (not recommended), or bind things to the various out fields
+// described below.
+//
+// controller.validate(hash) performs on-the-fly local validation of the users
+// input.
+//
+// Outs:
+//
+// controller.invalid: will be a hash of validation messages for any fields you
+// passed to login or recoverPassword - these might be returned from inside
+// CoginCtrl, or from your server.  Null = OK.  String value = validation
+// message.
+//
+// controller.statusType: 'error', 'progress', null
+//
+// controller.status: hash that includes 'message', 'progress' (0 to 1,
+// or 'indeterminate')
+//
+// Events - events are triggered on the scope.
+//
+
+angular.module('the-utils').controller('UtilsLoginCtrl', function(Api, $scope) {
+  var ctrl = this;
+  this.login = function(hash) {
+    return Api.login(hash).catch((x) => {
+      ctrl.statusType = 'error';
+      ctrl.status = { message: x.message };
+    });
+  };
+
+  this.recoverPassword = function(hash) {
+    return Api.recoverPassword(hash).catch((x) => {
+      ctrl.statusType = 'error';
+      ctrl.status = { message: x };
+    });
+  };
+
+  this.invalid = {};
+  this.statusType = null;
+  this.status = { message: 'Ready to log in!' };
+
+  var updateStatus = (type, hash) => {
+    this.statusType = type;
+    this.status = hash;
+  };
+
+  Api.on('status', updateStatus);
+  $scope.$on('$destroy', () => { Api.off('status', updateStatus); });
+
+});
