be037e46ab1fb819d3febb67b3064cc0bfac5d14
Daniel Staudigel <dstaudigel@gmail.com>
2015-12-05 11:47:22 -0800

    Compiled.

---
+++ index.js
@@ -0,0 +1,48 @@
+angular.module('the-common').service('Falter',function() {
+  var Falter = {
+    exception: function(exception,context) {
+      
+    },
+    network: function(networkError,context) {
+      
+    },
+    error: function(context) {
+      
+    }
+  };
+
+  return window.Falter = Falter;
+});
+angular.module('the-common').directive('statefulClick',function(Falter) {
+  return {
+    restrict: 'A',
+    /* Doesn't actually care about isolated scope, but these are
+    useful to know:
+    scope: { statefulClick: '&', statefulClass: '@'
+    },*/
+    link: function(scope,elem,attrs) {
+      function onClick(event) {
+        var cl = attrs.statefulClass || 'active';
+        elem.addClass(cl);
+        var ret = scope.$eval(attrs.statefulClick,{ $event : event });
+        
+        if(ret && ret.then) {
+          ret.then(function(res) {
+            elem.removeClass(cl);
+          },function(err) {
+            Error.error(err);
+            elem.removeClass(cl);
+          });
+        } else {
+          elem.removeClass(cl);
+        }
+      }
+      
+      elem.on('click',onClick);
+
+      scope.$on('$destroy',function() {
+        elem.off('click',onClick);
+      });
+    }
+  };
+});
