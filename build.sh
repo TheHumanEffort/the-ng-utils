c15d23c5a652fa4406893805da9f736dbb60605e
Daniel Staudigel <dstaudigel@gmail.com>
2015-12-05 12:01:48 -0800

    Fix the build script to work with multi-word messages.

--- build.sh
+++ build.sh
@@ -4,8 +4,16 @@ if [[ $# -lt 2 ]] ; then
 
     exit 0
 fi
-
+echo Compiling
 cat src/*.js > index.js
+echo Committing index
 git commit index.js -m "Compiled - $2"
-git tag -a $1 -m $2
+echo version bump in json
+node_modueles/.bin/json -I -f package.json -e 'this.version = "'$1'"'
+echo committing versoin bump
+git commit package.json -m "Version bump."
+echo tagging.. 
+git tag -a $1 -m "$2"
+echo pushing up
 git push origin master --follow-tags
+git push origin $1
