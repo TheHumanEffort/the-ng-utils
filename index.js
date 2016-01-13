95d52c9f5c04f0800418d317868ff2fa3d23feb9
Daniel Staudigel <dstaudigel@gmail.com>
2016-01-12 16:38:07 -0800

    Compiled.

--- index.js
+++ index.js
@@ -136,23 +136,24 @@ angular.module('the-utils').controller('UtilsSignupCtrl', function(Api, $scope) 
   $scope.$on('$destroy', () => { Api.off('status', updateStatus); });
 
 });
-angular.module('the-utils').directive('statefulClick',function(Falter) {
+angular.module('the-utils').directive('statefulClick', function(Falter) {
   return {
     restrict: 'A',
     /* Doesn't actually care about isolated scope, but these are
     useful to know:
     scope: { statefulClick: '&', statefulClass: '@'
     },*/
-    link: function(scope,elem,attrs) {
+    link: function(scope, elem, attrs) {
       function onClick(event) {
         var cl = attrs.statefulClass || 'active';
         elem.addClass(cl);
-        var ret = scope.$eval(attrs.statefulClick,{ $event : event });
+        var ret = scope.$eval(attrs.statefulClick, { $event: event });
 
-        if(ret && ret.then) {
+        if (ret && ret.then) {
           ret.then(function(res) {
             elem.removeClass(cl);
-          },function(err) {
+          }, function(err) {
+
             Error.error(err);
             elem.removeClass(cl);
           });
@@ -161,12 +162,12 @@ angular.module('the-utils').directive('statefulClick',function(Falter) {
         }
       }
 
-      elem.on('click',onClick);
+      elem.on('click', onClick);
 
-      scope.$on('$destroy',function() {
-        elem.off('click',onClick);
+      scope.$on('$destroy', function() {
+        elem.off('click', onClick);
       });
-    }
+    },
   };
 });
 angular.module("the-utils").run(["$templateCache",function($templateCache) {
